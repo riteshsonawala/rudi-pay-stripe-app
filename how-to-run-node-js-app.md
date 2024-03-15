548e8724fb516ec364f1b36032620b9ad2e750c0
Ritesh Sonawala <ritesh.sonawala@gmail.com>
2024-03-15 12:33:16 +0530

    Add Stripe API integration and running instructions
    Implemented Stripe API in test.js file with express app. This includes initialization of Stripe client, express app and two endpoints for retrieving charge data. Additionally, a markdown file has been created detailing steps to run a Node.js application, with specific instructions for setting up and running this project.

---
+++ how-to-run-node-js-app.md
@@ -0,0 +1,21 @@
+#### Project Setup
+
+Create a folder: Make a new project folder (e.g., stripe-api-demo)
+
+##### Initialize
+Open a terminal in this folder and run npm init -y to initialize a package.json file.
+
+Install Dependencies: Install the necessary packages with this command:
+`Bash
+npm init -y
+npm install stripe --save`
+
+Note: when you init it creates package.json, when you install stripe it adds the library version of stripe client
+
+#### Create Your Node.js Script (e.g., test.js)
+create file test.js under your project folder add following code to the file
+
+
+#### Run the Script
+`Bash
+node index.js`
---
+++ test/test.js
@@ -0,0 +1,38 @@
+const stripe = require('stripe');
+const {log} = require("debug");
+
+const express = require('express');
+// Initialize Stripe with your secret key
+const stripeClient = stripe("sk_test_51Jc7EcG0mjB1TJvND7xa2hf0A1tz5Zgo3gJCOB6vklOlYaoAgEGdMNni54Duz6fT1vxspJvrD2xWi1ZtUZr4hBhf00EJDxZIoi");
+
+// Create an Express app
+const app = express();
+const port = 3000; // Or any port you prefer
+
+// Endpoint to list recent charges
+app.get('/charges', async (req, res) => {
+    try {
+        const charges = await stripeClient.charges.list({
+            limit: 10 // Retrieve the last 10 charges (adjust as needed)
+        });
+
+        res.json(charges.data);
+
+    } catch (error) {
+        console.error('Error fetching charges:', error);
+        res.status(500).send({ error: 'Failed to retrieve charges' });
+    }
+});
+app.get('/richtest', async (req, res) => {
+    const charges =  await stripeClient.charges.list({
+        created: {gt: 1677672000, lt: 1678968000},
+        limit: 100
+    })
+    console.log(charges)
+});
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
